d55ba3da26d5fb9b2b925a68b357be985968926b
Andre <andrechaumetcutuli@gmail.com>
2023-07-20 09:19:44 -0300

    Implemented generic FontWriter (in progress)

---
+++ MyConsoleFonts/src/v2/FontWriter.java
@@ -0,0 +1,28 @@
+package v2;
+
+public class FontWriter<T extends Font> {
+
+    T font;
+
+    public String generateText(String text) {
+        char[] stringCharacters = text.toCharArray();
+        StringBuilder result = new StringBuilder("\n");
+        for(int rowNumber = 0; rowNumber < font.getAmountOfRows(); rowNumber++) {
+            for(char stringCharacter : stringCharacters) {
+                result.append(obtainFragment(stringCharacter, rowNumber));
+            }
+            result.append("\n");
+        }
+        return result.toString();
+    }
+
+    //TODO: Differ it from generateText(String);
+    public void writeText(String text) {
+        System.out.println(generateText(text));
+    }
+
+    private String obtainFragment(char character, int rowNumber) {
+        return font.getCharFragment(character, rowNumber);
+    }
+
+}
