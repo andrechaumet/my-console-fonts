28a0733755a22bb4adb86d9995d8643180291768
Andre <andrechaumetcutuli@gmail.com>
2023-07-23 00:56:19 -0300

    Minor fixes in Font generic Map generation

--- MyConsoleFonts/src/v2/Font.java
+++ MyConsoleFonts/src/v2/Font.java
@@ -5,26 +5,31 @@ import java.util.Map;
 
 public abstract class Font {
 
-    private final Map<Character, String[]> charactersMap;
+    private final Map<Character, String[]> CHARACTERS_MAP;
+    private final int AMOUNT_OF_ROWS;
 
     public Font(String[][] fontCharsMatrix, int amountOfRows) {
-        charactersMap = new HashMap<>(fontCharsMatrix.length);
+        CHARACTERS_MAP = new HashMap<>(fontCharsMatrix.length);
+        AMOUNT_OF_ROWS = amountOfRows;
         for (String[] rowsArray : fontCharsMatrix) {
             Character charKey = rowsArray[0].toUpperCase().charAt(0);
-            String[] stringValues = new String[amountOfRows];
+            String[] stringValues = new String[AMOUNT_OF_ROWS];
             for (int currentCharRow = 1; currentCharRow < stringValues.length; currentCharRow++) {
                 stringValues[currentCharRow - 1] = rowsArray[currentCharRow];
             }
-            charactersMap.put(charKey, stringValues);
+            CHARACTERS_MAP.put(charKey, stringValues);
         }
     }
 
     public String getCharFragment(char character, int rowNumber) {
-        return charactersMap.get(Character.toUpperCase(character))[rowNumber];
+        return CHARACTERS_MAP.get(Character.toUpperCase(character))[rowNumber];
     }
 
     public int getAmountOfChars() {
-        return charactersMap.size();
+        return CHARACTERS_MAP.size();
     }
 
+    public int getAmountOfRowsPerChar() {
+        return AMOUNT_OF_ROWS;
+    }
 }
